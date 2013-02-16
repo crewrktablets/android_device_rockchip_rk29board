957e56ab8821126aab055728e078b34d5569dd1e
netlars <netlars@gmx.de>
2013-02-16 21:19:49 +0100

    delete some code

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,22 +32,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/generic/common/bluetooth
 
 # WLAN
-#BOARD_WLAN_DEVICE := rtl8192cu
-#WPA_SUPPLICANT_VERSION := VER_0_8_X
-#BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
-#BOARD_HOSTAPD_DRIVER        := WEXT
-#BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_rtl
-#WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
-#WIFI_DRIVER_FW_PATH_PARAM   := ""
-#WIFI_DRIVER_FW_PATH_STA     := ""
-#WIFI_DRIVER_FW_PATH_AP      := ""
-#WIFI_DRIVER_FW_PATH_P2P     := ""
-#WIFI_DRIVER_MODULE_NAME     := wlan
-#WIFI_DRIVER_MODULE_ARG      := ""
-
-#TARGET_CUSTOM_WIFI := device/rockchip/rk29board/wifi_realtek.c
-
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
@@ -58,8 +42,8 @@ BOARD_WLAN_DEVICE           := bcmdhd
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcm4329.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/firmware/fw_bcm4329_p2p.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME     := "wlan"
+#WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
+#WIFI_DRIVER_MODULE_NAME     := "wlan"
 
 # Graphics
 BOARD_EGL_CFG := device/rockchip/rk29board/egl.cfg
